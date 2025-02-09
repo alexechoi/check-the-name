2710670dd6b7f35b8d1f45dc8c48d68be4a8975f
alexchvod <uceiac8@ucl.ac.uk>
2025-02-09 20:43:22 +0000

    SEO improvement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  generateBuildId: async () => 'build',
+  generateStaticParams: async () => {
+    return [
+      { slug: '' } // Home page
+    ];
+  },
+  async generateSitemaps() {
+    return [
+      {
+        url: 'https://checkthename.com',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      }
+    ];
+  }
 };
 
 export default nextConfig;
